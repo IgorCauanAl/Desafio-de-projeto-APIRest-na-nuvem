7d87047910370c95c6298825641f261449d3d451
Igor Cauan <igorcauawo@gmail.com>
2024-11-04 13:55:54 -0300

    Envio projeto

---
+++ src/test/java/edu/cursodio/edu/dio/criacaoAPI_REST/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cursodio.edu.dio.criacaoAPI_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
